be942078793e297f6174be10f2c12c8ae02576b6
nhan <nhan@codistry.io>
2018-04-05 19:59:57 -0700

    Added addItems functionality

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  addItem(e){
+    let itemArray = this.state.items;
+
+    if(this._inputElement.value !== ""){
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this.setState({
+        items: itemArray
+      });
+
+      this._inputElement.value ="";
+    }
+
+    console.log(itemArray);
+
+    e.preventDefault();
+  }
+
   render(){
     return(
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="enter task">
+          <form onSubmit={this.addItem}>
+            <input ref={ a => this._inputElement = a }
+                   placeholder="enter task">
             </input>
 
             <button type="submit">add</button>
