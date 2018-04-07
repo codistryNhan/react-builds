145d8c7f31f74b6c06eddafa373752a748d4b5c2
nhan <nhan@codistry.io>
2018-04-07 01:12:40 -0700

    Added React Flip Move for animations

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import TodoItems from './TodoItems';
+import './TodoList.css';
 
 class TodoList extends Component {
   constructor(props, context){
@@ -9,6 +11,7 @@ class TodoList extends Component {
     };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
 
   }
 
@@ -33,6 +36,16 @@ class TodoList extends Component {
     e.preventDefault();
   }
 
+  deleteItem(key) {
+    let filteredItems = this.state.items.filter( item => {
+      return (item.key !== key);
+    });
+
+    this.setState({
+      items: filteredItems
+    });
+  }
+
   render(){
     return(
       <div className="todoListMain">
@@ -45,6 +58,8 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items}
+                   delete={this.deleteItem}/>
       </div>
     );
   }
